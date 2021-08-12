7e41c08f5ad583e4547256d7f1f2e15795fb12a0
summer <82437499+summerlight636@users.noreply.github.com>
2021-08-12 11:57:35 +0900

    게임/유머 dto

---
+++ FS705/src/main/java/com/FS705/dto/GameCommentDTO.java
@@ -0,0 +1,67 @@
+package com.FS705.dto;
+
+public class GameCommentDTO {
+	private int cno, bno, no, clike, cdislike;
+	private String ccontent, cdate, name, id, cip;
+	public String getCip() {
+		return cip;
+	}
+	public void setCip(String cip) {
+		this.cip = cip;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getCno() {
+		return cno;
+	}
+	public void setCno(int cno) {
+		this.cno = cno;
+	}
+	public int getBno() {
+		return bno;
+	}
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public int getClike() {
+		return clike;
+	}
+	public void setClike(int clike) {
+		this.clike = clike;
+	}
+	public int getCdislike() {
+		return cdislike;
+	}
+	public void setCdislike(int cdislike) {
+		this.cdislike = cdislike;
+	}
+	public String getCcontent() {
+		return ccontent;
+	}
+	public void setCcontent(String ccontent) {
+		this.ccontent = ccontent;
+	}
+	public String getCdate() {
+		return cdate;
+	}
+	public void setCdate(String cdate) {
+		this.cdate = cdate;
+	}
+	
+}
---
+++ FS705/src/main/java/com/FS705/dto/HumorBoardDTO.java
@@ -0,0 +1,103 @@
+package com.FS705.dto;
+
+public class HumorBoardDTO {
+	private int bno, bcount, no, blike, bdislike, humorboardcount, humorcommentcount; 
+	private String btitle, bcontent, bdate, bcategory, bthumbnail, subCategory, bfile, id, name;
+	public int getBno() {
+		return bno;
+	}
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+	public int getBcount() {
+		return bcount;
+	}
+	public int getHumorboardcount() {
+		return humorboardcount;
+	}
+	public void setHumorboardcount(int humorboardcount) {
+		this.humorboardcount = humorboardcount;
+	}
+	public int getHumorcommentcount() {
+		return humorcommentcount;
+	}
+	public void setHumorcommentcount(int humorcommentcount) {
+		this.humorcommentcount = humorcommentcount;
+	}
+	public void setBcount(int bcount) {
+		this.bcount = bcount;
+	}
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public int getBlike() {
+		return blike;
+	}
+	public void setBlike(int blike) {
+		this.blike = blike;
+	}
+	public int getBdislike() {
+		return bdislike;
+	}
+	public void setBdislike(int bdislike) {
+		this.bdislike = bdislike;
+	}
+	public String getBtitle() {
+		return btitle;
+	}
+	public void setBtitle(String btitle) {
+		this.btitle = btitle;
+	}
+	public String getBcontent() {
+		return bcontent;
+	}
+	public void setBcontent(String bcontent) {
+		this.bcontent = bcontent;
+	}
+	public String getBdate() {
+		return bdate;
+	}
+	public void setBdate(String bdate) {
+		this.bdate = bdate;
+	}
+	public String getBcategory() {
+		return bcategory;
+	}
+	public void setBcategory(String bcategory) {
+		this.bcategory = bcategory;
+	}
+	public String getBthumbnail() {
+		return bthumbnail;
+	}
+	public void setBthumbnail(String bthumbnail) {
+		this.bthumbnail = bthumbnail;
+	}
+	public String getSubCategory() {
+		return subCategory;
+	}
+	public void setSubCategory(String subCategory) {
+		this.subCategory = subCategory;
+	}
+	public String getBfile() {
+		return bfile;
+	}
+	public void setBfile(String bfile) {
+		this.bfile = bfile;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
