012c206e7a2ca1f47f18b69349efa3adf2cf9c8f
jeeankwon <81234879+jeeankwon@users.noreply.github.com>
2021-08-18 22:42:42 +0900

    sports

---
+++ FS705/src/main/java/com/FS705/dto/CommentDTO.java
@@ -0,0 +1,89 @@
+package com.FS705.dto;
+
+public class CommentDTO {
+	
+	private int cno, clike, cdislike, bno, no;
+	
+	private String ccontent, cip, cdate, name, id;
+
+	public int getCno() {
+		return cno;
+	}
+
+	public void setCno(int cno) {
+		this.cno = cno;
+	}
+
+	public int getClike() {
+		return clike;
+	}
+
+	public void setClike(int clike) {
+		this.clike = clike;
+	}
+
+	public int getCdislike() {
+		return cdislike;
+	}
+
+	public void setCdislike(int cdislike) {
+		this.cdislike = cdislike;
+	}
+
+	public int getBno() {
+		return bno;
+	}
+
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getCcontent() {
+		return ccontent;
+	}
+
+	public void setCcontent(String ccontent) {
+		this.ccontent = ccontent;
+	}
+
+	public String getCip() {
+		return cip;
+	}
+
+	public void setCip(String cip) {
+		this.cip = cip;
+	}
+
+	public String getCdate() {
+		return cdate;
+	}
+
+	public void setCdate(String cdate) {
+		this.cdate = cdate;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+}
