fae0eac91c0f1e2b9c13de27ba9a05f5c220af3b
parkjunyoung <hpaaycim1@naver.com>
2021-08-20 14:25:55 +0900

    Add files via upload

--- FS705/src/main/java/com/FS705/web/Error.java
+++ FS705/src/main/java/com/FS705/web/Error.java
@@ -8,11 +8,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import com.FS705.dao.LogDAO;
-import com.FS705.dto.LogDTO;
-import com.FS705.util.Util;
 
 @WebServlet("/error")
 public class Error extends HttpServlet {
@@ -23,22 +18,6 @@ public class Error extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
-
-		String id = "";
-		if(session.getAttribute(id) != null) {
-			id = (String)session.getAttribute("id");
-		}
-		
-		LogDTO logDto = new LogDTO();
-				
-		logDto.setLogIp(Util.getIP(request));
-		logDto.setLogTarget("error");
-		logDto.setLogdId((String)session.getAttribute(id));
-		logDto.setLogEtc(request.getHeader("User-Agent"));
-		logDto.setLogMethod("post");
-		LogDAO.insertLog(logDto);
-		
 		RequestDispatcher rd = request.getRequestDispatcher("./error.jsp");
 		request.setAttribute("code", request.getParameter("code"));
 		rd.forward(request, response);
